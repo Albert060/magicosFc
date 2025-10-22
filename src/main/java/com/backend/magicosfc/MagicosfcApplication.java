399df6f627ee89b198d2278307b1dd7108bac064
Albert060 <albertjavendanog@gmail.com>
2025-10-22 18:53:01 +0200

    inicializar

---
+++ src/main/java/com/backend/magicosfc/MagicosfcApplication.java
@@ -0,0 +1,13 @@
+package com.backend.magicosfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicosfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicosfcApplication.class, args);
+	}
+
+}
